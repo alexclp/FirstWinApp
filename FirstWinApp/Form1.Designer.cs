f5720f6c3fc42023c1609610ca6ff739d114deee
Alexandru Clapa <alexandru.clapa@gmail.com>
2013-10-14 12:54:08 +0300

    Updated content

--- FirstWinApp/Form1.Designer.cs
+++ FirstWinApp/Form1.Designer.cs
@@ -40,9 +40,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(57, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Iti place ora?";
+            this.label1.Text = "Stoica e gay?";
             // 
             // radioButton1
             // 
